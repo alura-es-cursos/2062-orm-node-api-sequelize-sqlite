7106c28b82fda2567d3d23d90ce6de0c610ff0b8
Leonardo Jose <ljcl79@gmail.com>
2024-01-14 23:12:25 -0300

    Aula2

---
+++ src/models/persona.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Persona extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Persona.init({
+    nombre: DataTypes.STRING,
+    email: DataTypes.STRING,
+    dni: DataTypes.STRING,
+    activo: DataTypes.BOOLEAN,
+    rol: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Persona',
+    tableName: 'personas'
+  });
+  return Persona;
+};

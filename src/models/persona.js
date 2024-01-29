5f895817f136871de7dda347b9df338c61553f36
Leonardo Jose <ljcl79@gmail.com>
2024-01-29 01:28:38 -0300

    Aula4

--- src/models/persona.js
+++ src/models/persona.js
@@ -4,13 +4,15 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Persona extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+
     static associate(models) {
-      // define association here
+      Persona.hasMany(models.Curso, {
+        foreignKey: 'docente_id',
+      });
+
+      Persona.hasMany(models.Matricula, {
+        foreignKey: 'estudiante_id',
+      });
     }
   }
   Persona.init({
